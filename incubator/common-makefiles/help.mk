731d7ecc3cac58aea82d3f6e1d1293229f487e82
boosh <boosh@sugarkube.io>
2018-08-21 21:39:49 +0100

    - Update help

--- incubator/common-makefiles/help.mk
+++ incubator/common-makefiles/help.mk
@@ -1,11 +1,11 @@
 .PHONY: clean
 help:
-	@echo To install standard kapps, run \"make install\" with the following env vars:
+	@echo To install standard kapps, run \"make install\" with the following vars \(env vars are in capitals\):
 	@echo
-	@echo Mandatory env vars:
+	@echo Mandatory:
 	@echo \* APPROVED=\<false/true\> - Run first with \'APPROVED=false\' to plan then \'APPROVED=true\' to actually install.
 	@echo
-	@echo Optional env vars (most have default values):
+	@echo Optional \(most have default values\):
 	@echo \* CLUSTER=\<cluster\> - name of your target cluster
 	@echo \* CLUSTER_PROFILE=\<cluster_profile\> - name of your stack cluster profile
 	@echo
@@ -13,6 +13,7 @@ help:
 	@echo \* REGION=\<region\>
 	@echo
 	@echo Helm-specific:
+	@echo \* helm-opts=\<helm options\> - e.g. \'make all helm-opts=\"-f values-\<env\>.yaml\"\'. Not an env var, pass as a parameter.
 	@echo \* KUBE_CONTEXT=\<context\> - Name of the kube context to use
 	@echo \* NAMESPACE=\<namespace\> - Helm namespace to install into
 	@echo \* RELEASE=\<release\> - Helm release name
