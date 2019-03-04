b71acddebde795e2043f8d2ec22eb788f6ebcb7e
boosh <boosh@sugarkube.io>
2019-03-04 18:46:18 +0000

    - Rename variables in the hosted-zone kapp - Add a sugarkube.yaml file to it

--- incubator/hosted-zone/terraform_aws/main.tf
+++ incubator/hosted-zone/terraform_aws/main.tf
@@ -1,25 +1,21 @@
-locals {
-  cluster_zone = "${var.cluster}.${var.parent_zone}"
+resource "aws_route53_zone" "cluster_hosted_zone" {
+  name = "${var.cluster_name}"
 }
 
-resource "aws_route53_zone" "cluster_zone" {
-  name = "${local.cluster_zone}"
-}
-
-data "aws_route53_zone" "parent_zone" {
-  name = "${var.parent_zone}"
+data "aws_route53_zone" "parent_hosted_zone" {
+  name = "${var.parent_hosted_zone}"
 }
 
 resource "aws_route53_record" "parent" {
-  zone_id = "${data.aws_route53_zone.parent_zone.zone_id}"
-  name    = "${local.cluster_zone}"
+  zone_id = "${data.aws_route53_zone.parent_hosted_zone.zone_id}"
+  name    = "${var.cluster_name}"
   type    = "NS"
   ttl     = "${var.ns_ttl}"
 
   records = [
-    "${aws_route53_zone.cluster_zone.name_servers.0}",
-    "${aws_route53_zone.cluster_zone.name_servers.1}",
-    "${aws_route53_zone.cluster_zone.name_servers.2}",
-    "${aws_route53_zone.cluster_zone.name_servers.3}",
+    "${aws_route53_zone.cluster_hosted_zone.name_servers.0}",
+    "${aws_route53_zone.cluster_hosted_zone.name_servers.1}",
+    "${aws_route53_zone.cluster_hosted_zone.name_servers.2}",
+    "${aws_route53_zone.cluster_hosted_zone.name_servers.3}",
   ]
 }
--- incubator/hosted-zone/terraform_aws/variables.tf
+++ incubator/hosted-zone/terraform_aws/variables.tf
@@ -1,9 +1,13 @@
-variable "cluster" {
+variable "cluster_name" {
   description = "Name of the cluster"
 }
 
-variable "parent_zone" {
+variable "parent_hosted_zone" {
   description = "Name of the parent hosted zone to create NS records in for the zone for this cluster"
+}
+
+variable "region" {
+  description = "AWS region to use"
 }
 
 variable "ns_ttl" {
