148f5268442438b9c31ee6200f3343de0bf2d994
boosh <boosh@sugarkube.io>
2019-03-28 06:59:11 +0000

    - Delete directories starting with _generated in the 'clean' target

--- incubator/common-makefiles/clean.mk
+++ incubator/common-makefiles/clean.mk
@@ -2,4 +2,5 @@
 clean: tf-clean
 	-rm *.log
 	-rm *.err
-	-find . -name '_generated_*' -delete
+	-find . -name '_generated_*' -type f -delete
+	-find . -name '_generated_*' -type d -exec rm -r {} ';'
