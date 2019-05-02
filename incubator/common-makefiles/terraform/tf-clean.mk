494f22bdc01ac4174df37893bb835fc787cd6f48
boosh <boosh@sugarkube.io>
2019-05-02 18:21:29 +0100

    - Fix cleaning terraform

--- incubator/common-makefiles/terraform/tf-clean.mk
+++ incubator/common-makefiles/terraform/tf-clean.mk
@@ -1,9 +1,10 @@
 .PHONY: tf-clean
 tf-clean:
 	if [ -d "$(TERRAFORM_DIR)" ]; then \
-		rm -rf $(TERRAFORM_DIR)/.terraform ;\
-		rm $(TERRAFORM_DIR)/*.tfplan* ;\
-		rm $(TERRAFORM_DIR)/_generated_* ;\
+		rm -rf $(TERRAFORM_DIR)/.terraform || true ;\
+		rm $(TERRAFORM_DIR)/*.tfplan* || true ;\
+		rm $(TERRAFORM_DIR)/_generated_* || true ;\
+		find $(TERRAFORM_DIR) -name '_generated_*.tfvars' -type f -delete || true ;\
 	else \
 		echo [$@] No $(TERRAFORM_DIR) directory, skipping... ;\
 	fi
