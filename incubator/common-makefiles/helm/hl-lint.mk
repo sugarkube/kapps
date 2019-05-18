b66f17c5d856613d04aa50fe48894a167e661eed
boosh <boosh@sugarkube.io>
2019-05-18 20:52:09 +0100

    - Don't require a values.yaml file to lint helm charts

--- incubator/common-makefiles/helm/hl-lint.mk
+++ incubator/common-makefiles/helm/hl-lint.mk
@@ -3,7 +3,6 @@ hl-lint:
 	if [ ! -z "$(KUBE_CONTEXT)" ] && [ ! -z "$(HELM)" ] && [ -f "$(CHART_DIR)/Chart.yaml" ]; then \
 		KUBECONFIG=$(KUBECONFIG) $(HELM) lint --kube-context=$(KUBE_CONTEXT) \
 			$(CHART_DIR) \
-			-f values.yaml \
 			--namespace=$(NAMESPACE) \
 			$(helm-params) \
 			$(local-helm-opts) ;\
