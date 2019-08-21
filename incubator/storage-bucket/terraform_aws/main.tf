778bcfcc6c9e113500c7391fc105e48e9f941ff6
boosh <boosh@sugarkube.io>
2019-08-21 08:18:31 +0100

    - Add terraform configs to create an S3 bucket

--- incubator/storage-bucket/terraform_aws/main.tf
+++ incubator/storage-bucket/terraform_aws/main.tf
@@ -1,6 +1,19 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.bucket_name}"
-  acl    = "private"
-}
+  acl = "private"
 
-// todo - create a kapp that creates KMS keys and enable server-side encryption on this bucket
+  tags = {
+    Name = "${var.bucket_name}"
+    Cluster = "${var.cluster}"
+    Sugarkube = "true"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${var.encryption_key_id}"
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
+}
--- incubator/storage-bucket/terraform_aws/variables.tf
+++ incubator/storage-bucket/terraform_aws/variables.tf
@@ -1,3 +1,11 @@
 variable "bucket_name" {
   description = "Name of the S3 bucket to create"
 }
+
+variable "cluster" {
+  description = "Unique per cluster. Allows multiple clusters per cloud-provider account. Used for namespacing."
+}
+
+variable "encryption_key_id" {
+  description = "KMS key ARN"
+}
