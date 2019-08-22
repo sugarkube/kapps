f8ae5a96c512cd6ac1d642049e1f8014550faeda
boosh <boosh@sugarkube.io>
2019-08-22 10:19:06 +0100

    - Block public access to the state bucket

--- incubator/terraform-bucket/terraform_aws/main.tf
+++ incubator/terraform-bucket/terraform_aws/main.tf
@@ -5,11 +5,11 @@ resource "aws_kms_key" "key" {
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.bucket_name}"
-  acl = "private"
+  acl    = "private"
 
   tags = {
-    Name = "${var.bucket_name}"
-    Cluster = "${var.cluster}"
+    Name      = "${var.bucket_name}"
+    Cluster   = "${var.cluster}"
     Sugarkube = "true"
   }
 
@@ -17,8 +17,17 @@ resource "aws_s3_bucket" "bucket" {
     rule {
       apply_server_side_encryption_by_default {
         kms_master_key_id = "${aws_kms_key.key.arn}"
-        sse_algorithm = "aws:kms"
+        sse_algorithm     = "aws:kms"
       }
     }
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = "${aws_s3_bucket.bucket.id}"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
--- incubator/terraform-bucket/terraform_aws/outputs.tf
+++ incubator/terraform-bucket/terraform_aws/outputs.tf
@@ -1,1 +1,5 @@
-kms_key_arn = "${aws_kms_key.key.arn}"
+// this is generically named so it makes sense if we add support for e.g. azure, gce, etc.
+output "bucket_encryption_key" {
+  value       = "${aws_kms_key.key.arn}"
+  description = "ID of the key used to encrypt data in the bucket"
+}
