e7959d01b897743f20cd51addb436768a91949e0
boosh <boosh@sugarkube.io>
2019-09-04 14:29:52 +0100

    Add more outputs

--- incubator/auroradb/terraform_aws/outputs.tf
+++ incubator/auroradb/terraform_aws/outputs.tf
@@ -6,6 +6,10 @@ output "endpoint" {
   value = "${aws_rds_cluster.db.endpoint}"
 }
 
-output "port" {
-  value = "${aws_rds_cluster.db.port}"
+output "master_user" {
+  value = "${var.master_username}"
+}
+
+output "database" {
+  value = "${var.database_name}"
 }
