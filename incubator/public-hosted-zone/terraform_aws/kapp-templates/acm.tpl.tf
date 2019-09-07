532c0201c8110b160e074d7267ddcba2589b8081
boosh <boosh@sugarkube.io>
2019-09-07 07:33:09 +0100

    - Make creation of ACM certs optional based on the value of a kapp var

--- incubator/public-hosted-zone/terraform_aws/kapp-templates/acm.tpl.tf
+++ incubator/public-hosted-zone/terraform_aws/kapp-templates/acm.tpl.tf
@@ -1,3 +1,5 @@
+{{ if .kapp.vars.create_acm_certs }}
+// create a wildcard ACM cert
 resource "aws_acm_certificate" "cert" {
   domain_name       = "*.${var.hosted_zone}"
   validation_method = "DNS"
@@ -23,3 +25,4 @@ resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = "${aws_acm_certificate.cert.arn}"
   validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
 }
+{{ end }}
