d14e137660be26fe1e5908bb8f5d2b8fd89294a0
boosh <boosh@sugarkube.io>
2019-09-11 12:35:36 +0100

    - Working on creating a VPN endpoint

--- incubator/aws-vpn/scripts/main.py
+++ incubator/aws-vpn/scripts/main.py
@@ -18,6 +18,8 @@ AWS=os.getenv("AWS", "aws")       # path to the AWS CLI binary
 CFSSL=os.getenv("CFSSL", "cfssl")       # path to the cfssl binary
 CFSSL_JSON=os.getenv("CFSSL_JSON", "cfssljson")       # path to the cfssljson binary
 
+CLIENT="client"
+SERVER="server"
 
 def main():
     parser = argparse.ArgumentParser(description='Creates/updates hosted zones for kops.')
@@ -28,18 +30,41 @@ def main():
 
     args = parser.parse_args()
 
+    cluster_name = args.cluster_name
+
     out_dir = args.out_dir
     out_dir = os.path.abspath(out_dir)
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
-    generate_certs(cluster_name=args.cluster_name,
+    generate_certs(cluster_name=cluster_name,
                    cert_json=args.cert_json,
                    out_dir=out_dir,
                    hostnames=args.hostnames)
 
-    import_certs(out_dir=out_dir,
-                 cluster_name=args.cluster_name)
+    cert_arns = import_certs(out_dir=out_dir,
+                             cluster_name=cluster_name)
+
+    create_vpn_endpoint(cert_arns=cert_arns,
+                        cluster_name=cluster_name)
+
+
+def create_vpn_endpoint(cert_arns, cluster_name):
+    print("Creating VPN endpoint...")
+    command = '%s ec2 create-client-vpn-endpoint --client-cidr-block %s --server-certificate-arn %s ' \
+              '--authentication-options %s --connection-log-options Enabled=false --dns-servers %s ' \
+              '--split-tunnel --description "%s" ' \
+              '--tag-specifications \'ResourceType=client-vpn-endpoint,Tags=[{Key=Name,Value="%s"}]\'' % (
+        AWS,
+        '172.20.0.0/16',                # todo - grab the correct CIDR range for the VPC
+        cert_arns[SERVER],
+        'Type=certificate-authentication,MutualAuthentication={ClientRootCertificateChainArn=%s}' % cert_arns[CLIENT],
+        '172.20.0.2',                   # todo - grab the correct CIDR range for the VPC
+        "%s VPN" % cluster_name,
+        "%s VPN" % cluster_name)
+    logging.info("Executing command: %s" % command)
+    result = subprocess.run(command, shell=True, check=True, capture_output=True)
+    logging.info("Got output: %s" % result)
 
 
 def import_certs(out_dir, cluster_name):
@@ -50,7 +75,7 @@ def import_certs(out_dir, cluster_name):
     """
     arns = {}
 
-    for actor in ['client', 'server']:
+    for actor in [CLIENT, SERVER]:
         command = '%s acm import-certificate --certificate file://%s/%s.pem --private-key file://%s/%s-key.pem ' \
                   '--certificate-chain file://%s/ca.pem --output=text' %(AWS, out_dir, actor, out_dir, actor, out_dir)
         logging.info("Executing command in %s: %s" % (out_dir, command))
@@ -69,6 +94,7 @@ def import_certs(out_dir, cluster_name):
         print('='.join([k, v]))
 
     print()
+    return arns
 
 
 def generate_certs(cluster_name, cert_json, out_dir, hostnames=""):
@@ -86,7 +112,7 @@ def generate_certs(cluster_name, cert_json, out_dir, hostnames=""):
     subprocess.run(command, shell=True, check=True, cwd=out_dir)
 
     # generate client/server certs
-    for actor in ['client', 'server']:
+    for actor in [CLIENT, SERVER]:
         # command = '%s gencert -initca -cn="%s VPN CA" %s | %s -bare ca' % (CFSSL, cluster_name, cert_json, CFSSL_JSON)
         command = '%s gencert -ca=ca.pem -cn="%s VPN %s" -hostname="%s" -ca-key=%s %s | %s -bare %s' % (
             CFSSL,
