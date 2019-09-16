3582dac9e459fc0a84c72513c9e246047f6a7c95
boosh <boosh@sugarkube.io>
2019-09-16 15:28:27 +0100

    Add a script to delete the Prometheus CRDs

---
+++ incubator/prometheus-operator/delete-crds.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+$KUBECTL --context=$KUBE_CONTEXT delete crd prometheuses.monitoring.coreos.com
+$KUBECTL --context=$KUBE_CONTEXT delete crd prometheusrules.monitoring.coreos.com
+$KUBECTL --context=$KUBE_CONTEXT delete crd servicemonitors.monitoring.coreos.com
+$KUBECTL --context=$KUBE_CONTEXT delete crd podmonitors.monitoring.coreos.com
+$KUBECTL --context=$KUBE_CONTEXT delete crd alertmanagers.monitoring.coreos.com
