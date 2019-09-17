23e9f87e06def49352fce4e0cc16e334940b8f0e
boosh <boosh@sugarkube.io>
2019-09-17 10:27:05 +0100

    - Pass the KUBECONFIG to the script that deletes the Prometheus Operator CRDs

--- incubator/prometheus-operator/delete-crds.sh
+++ incubator/prometheus-operator/delete-crds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
-$KUBECTL --context=$KUBE_CONTEXT delete crd prometheuses.monitoring.coreos.com
-$KUBECTL --context=$KUBE_CONTEXT delete crd prometheusrules.monitoring.coreos.com
-$KUBECTL --context=$KUBE_CONTEXT delete crd servicemonitors.monitoring.coreos.com
-$KUBECTL --context=$KUBE_CONTEXT delete crd podmonitors.monitoring.coreos.com
-$KUBECTL --context=$KUBE_CONTEXT delete crd alertmanagers.monitoring.coreos.com
+KUBECONFIG=$KUBECONFIG $KUBECTL --context=$KUBE_CONTEXT delete crd prometheuses.monitoring.coreos.com
+KUBECONFIG=$KUBECONFIG $KUBECTL --context=$KUBE_CONTEXT delete crd prometheusrules.monitoring.coreos.com
+KUBECONFIG=$KUBECONFIG $KUBECTL --context=$KUBE_CONTEXT delete crd servicemonitors.monitoring.coreos.com
+KUBECONFIG=$KUBECONFIG $KUBECTL --context=$KUBE_CONTEXT delete crd podmonitors.monitoring.coreos.com
+KUBECONFIG=$KUBECONFIG $KUBECTL --context=$KUBE_CONTEXT delete crd alertmanagers.monitoring.coreos.com
